0c2ddab56f142e53535044876f0c8c81cb9b8061
SulaimanMansaray335 <Sulaimant.mansaray@gmail.com>
2026-01-10 20:56:31 -0500

    adding model transformation

--- setup.py
+++ setup.py
@@ -14,8 +14,8 @@ def get_requirements(file_path:str)->List[str]:
     requirements = []
     with open(file_path) as file_obj:
         requirements = file_obj.readlines()
-        requirements = [req.replace("\n", "") for req in requirements]
-
+        requirements = [req.strip() for req in requirements]
+        #requirements = [req.replace("\n", "") for req in requirements]
         if HYPHEN_E_DOT in requirements:
             requirements.remove(HYPHEN_E_DOT)
 
@@ -27,7 +27,7 @@ version = '0.0.1',
 author = 'tariq',
 author_email = 'tariq.mansaray@gmail.com',
 packages = find_packages(),
-requires = get_requirements('requirements.txt')
+install_requires = get_requirements('requirements.txt')
 
 
 
